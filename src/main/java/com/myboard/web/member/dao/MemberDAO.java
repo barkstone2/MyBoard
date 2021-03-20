631dbcbe299864903253c2fb32f17b755b221e87
barkstone2 <barkstone2@gmail.com>
2021-03-20 20:42:46 +0900

    #24 회원 모듈 DB처리
    DTO 클래스 추가.
    Mybatis 쿼리문 작성.
    Service, DAO 인터페이스 추가

---
+++ src/main/java/com/myboard/web/member/dao/MemberDAO.java
@@ -0,0 +1,20 @@
+package com.myboard.web.member.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.myboard.web.member.entity.MemberDTO;
+
+@Mapper
+public interface MemberDAO {
+
+	int insert(MemberDTO dto);
+
+	int login(String id, String pwd);
+
+	int loginChk(String id);
+
+	MemberDTO getView(int cookNo);
+
+	int update(MemberDTO dto);
+
+}
---
+++ src/main/java/com/myboard/web/member/service/MemberService.java
@@ -0,0 +1,17 @@
+package com.myboard.web.member.service;
+
+import com.myboard.web.member.entity.MemberDTO;
+
+public interface MemberService {
+
+	int insert(MemberDTO dto);
+
+	int login(String id, String pwd);
+
+	int loginChk(String id);
+
+	MemberDTO getView(int cookNo);
+
+	int update(MemberDTO dto);
+
+}
